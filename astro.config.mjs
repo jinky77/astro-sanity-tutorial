ae27817f3c9b793ba7d7ced15e109bca1e5288e1
jinky77 <paul.tinkerlabs@gmail.com>
2025-12-07 15:05:58 +0100

    finalized tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,30 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
-export default defineConfig({});
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [
+    sanity({
+      projectId: "kzn3z33b",
+      dataset: "production",
+      useCdn: false,
+      apiVersion: "2025-12-07",
+      studioBasePath: "/studio",
+    }),
+    react(),
+  ],
+
+  optimizeDeps: {
+    exclude: ["react-compiler-runtime", "lodash/startCase.js"],
+  },
+
+  adapter: cloudflare({ imageService: "compile" }),
+});
